1470d628f70f8aa8896d96b82ea2b11be32d41f7
Anand Chaudhary <anand92490@gmail.com>
2019-09-19 16:37:42 -0500

    Controller routes  setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Eat-da-Burger App
+# 🍔 Eat-da-Burger!
 
  Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.
 
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,18 +3,69 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burgers = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res){
-    burger:all(function(req, res){
-        var hbsObject = {burgers:data};
-        console.log(hbsObject);
+router.get("*", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
         res.render("index", hbsObject);
+    });
+});
+
+
+// Index Page (render all burgers to DOM)
+router.get("/index", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = { burgers: data };
+        // console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//Create new burger
+
+router.post("/api/new", function (req, res) {
+    burger.insertOne(["burger_name"], [req.body.name], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+
+//devour new burger
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+ // console.log("condition", condition);
+ 
+ burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result){
+        if(result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
 
 
 
+//export router to server.js
 module.exports = router;
