b7fa8640dca5cb177e0332c6b9fd376cef6fcce0
Anand Chaudhary <anand92490@gmail.com>
2019-09-18 22:32:37 -0500

    Server setup. Connected to burgers_controller.js route.

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burgers = require("../models/burger.js");
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set("view engine", "handlebars");
 
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/_controller.js");
+var routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
